7f12cfb84109ce9436f56748d78b694a578d894e
Richard Iannone <riannone@me.com>
2019-10-07 23:42:38 -0400

    Add examples

--- R/SVG.R
+++ R/SVG.R
@@ -1,7 +1,7 @@
 #' Create an `svg` object
 #'
 #' The `SVG()` function is the entry point for building an SVG from the ground
-#' up. We can provided predefined `height` and `width` attributes that define
+#' up. We can provide predefined `height` and `width` attributes that define
 #' the canvas size for the SVG. From here, we would want to use functions that
 #' add elements to the SVG object (e.g., [svg_rect()], [svg_circle()], etc.) and
 #' thus progressively build the graphic.
@@ -9,6 +9,23 @@
 #' @param width,height The dimensions of the SVG in `px` units.
 #' @param title The `<title>` tag for the finalized SVG.
 #' @param desc The `<desc>` tag for the finalized SVG.
+#'
+#' @examples
+#' # Create an SVG with nothing drawn
+#' # within it
+#' svg <- SVG(width = 200, height = 100)
+#'
+#' # Add a rectangle and then a circle
+#' svg <-
+#'   svg %>%
+#'   svg_rect(
+#'     x = 20, y = 20,
+#'     width = 40, height = 40
+#'   ) %>%
+#'   svg_circle(
+#'     x = 100, y = 40,
+#'     diameter = 40
+#'   )
 #'
 #' @export
 SVG <- function(width,
