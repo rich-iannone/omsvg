ca275b8caaaf93d39c56df840144222279163cf8
Richard Iannone <riannone@me.com>
2020-04-13 12:04:57 -0400

    Update la_process.R

--- data-raw/la_process.R
+++ data-raw/la_process.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 library(omsvg)
-library(here)
 library(usethis)
 
 file_list <- list.files("data-raw/svg", full.names = TRUE)
 
+solids <- file_list[stringr::str_detect(file_list, pattern = "-solid.svg$")]
+solids_new <- gsub("-solid.svg", ".svg", solids)
+
+file.rename(from = solids, to = solids_new)
+
+file_list <- list.files("data-raw/svg", full.names = TRUE)
 icon_names <- list.files("data-raw/svg") %>% gsub(".svg", "", .)
 
 svg_text <- rep(NA_character_, length(file_list))
@@ -13,7 +18,15 @@ for (i in seq(file_list)) {
 }
 
 # Create a table of LA SVG data
-la_svg_tbl <- dplyr::tibble(icon_names = icon_names, svg_text = svg_text)
+la_svg_tbl <- dplyr::tibble(icon_name = icon_names, svg_text = svg_text)
+
+# Read in the lookup table for icon names and categories
+categories_icons <- readr::read_csv("data-raw/categories_icons.csv", col_types = "cc")
+
+# Join tables together to get a `category_column`
+la_svg_tbl <-
+  la_svg_tbl %>%
+  dplyr::left_join(categories_icons, by = "icon_name")
 
 # Save to internal data
 usethis::use_data(la_svg_tbl, internal = TRUE, overwrite = TRUE)
