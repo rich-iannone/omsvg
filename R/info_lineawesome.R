488a9b6bcef89ecb89d25c8bcd12ebc9721cd819
Richard Iannone <riannone@me.com>
2020-04-14 16:55:25 -0400

    Create info_lineawesome.R

---
+++ R/info_lineawesome.R
@@ -0,0 +1,13 @@
+#' Get an information table showing all Line Awesome icons
+#'
+#' This informative table shows which Line Awesome icons are available inside of
+#' **omsvg**. The icons are composed of lines and they look awesome! There are
+#' plenty to choose from also, nearly *1400* icons across *69* categories. Just
+#' take note of the ones you like and get their names, you'll need them when
+#' using the [SVG_la()] function.
+#'
+#' @export
+info_lineawesome <- function() {
+
+  htmltools::html_print(htmltools::HTML(gt_tbl_raw))
+}
