717d37d00eb6ecc3ec5712e45df1537303799ac6
Richard Iannone <riannone@me.com>
2021-02-12 13:49:10 -0500

    Update info_lineawesome.R

--- R/info_lineawesome.R
+++ R/info_lineawesome.R
@@ -6,8 +6,13 @@
 #' take note of the ones you like and get their names, you'll need them when
 #' using the [SVG_la()] function.
 #'
+#' @return Invisibly returns `NULL`. The side effect of displaying a table of
+#'   icons is the purpose of this function.
+#'
 #' @export
 info_lineawesome <- function() {
 
   htmltools::html_print(htmltools::HTML(gt_tbl_raw))
+
+  invisible(NULL)
 }
